6fd30d123b08b789b0fd375fe54e32966e7d8e1a
alejandro.caballero <alejandro.caballero@infometrika.com>
2026-04-05 20:11:37 -0500

    feature: Ajustes del evaluador para que funcione correctamente con números de más de un dígito y que acepte números decimales

--- ExpressionEvaluator.Core/Evaluator.cs
+++ ExpressionEvaluator.Core/Evaluator.cs
@@ -1,5 +1,7 @@
 ﻿namespace ExpressionEvaluator.Core;
 
+using System.Globalization;
+
 public class Evaluator
 {
     public static double Evaluate(string infix)
@@ -10,50 +12,68 @@ public class Evaluator
 
     private static string InfixToPostfix(string infix)
     {
-        var postFix = string.Empty;
+        var postFix = new List<string>();
         var stack = new Stack<char>();
-        foreach (var item in infix)
+        foreach (var item in Tokenize(infix))
         {
-            if (IsOperator(item))
+            if (item.Length == 1 && IsOperator(item[0]))
             {
+                var currentOperator = item[0];
+
+                if (currentOperator == '(')
+                {
+                    stack.Push(currentOperator);
+                    continue;
+                }
+
+                if (currentOperator == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                    {
+                        postFix.Add(stack.Pop().ToString());
+                    }
+
+                    if (stack.Count == 0 || stack.Peek() != '(')
+                    {
+                        throw new Exception("Sintax error.");
+                    }
+
+                    stack.Pop();
+                    continue;
+                }
+
                 if (stack.Count == 0)
                 {
-                    stack.Push(item);
+                    stack.Push(currentOperator);
                 }
                 else
                 {
-                    if (item == ')')
+                    while (stack.Count > 0 && PriorityInfix(currentOperator) <= PriorityStack(stack.Peek()))
                     {
-                        do
-                        {
-                            postFix += stack.Pop();
-                        } while (stack.Peek() != '(');
-                        stack.Pop();
+                        postFix.Add(stack.Pop().ToString());
                     }
-                    else
-                    {
-                        if (PriorityInfix(item) > PriorityStack(stack.Peek()))
-                        {
-                            stack.Push(item);
-                        }
-                        else
-                        {
-                            postFix += stack.Pop();
-                            stack.Push(item);
-                        }
-                    }
+
+                    stack.Push(currentOperator);
                 }
             }
             else
             {
-                postFix += item;
+                postFix.Add(item);
             }
         }
+
         while (stack.Count > 0)
         {
-            postFix += stack.Pop();
+            var item = stack.Pop();
+            if (item == '(')
+            {
+                throw new Exception("Sintax error.");
+            }
+
+            postFix.Add(item.ToString());
         }
-        return postFix;
+
+        return string.Join(' ', postFix);
     }
 
     private static int PriorityStack(char item) => item switch
@@ -81,13 +101,13 @@ public class Evaluator
     private static double EvaluatePostfix(string postfix)
     {
         var stack = new Stack<double>();
-        foreach (char item in postfix)
+        foreach (var item in postfix.Split(' ', StringSplitOptions.RemoveEmptyEntries))
         {
-            if (IsOperator(item))
+            if (item.Length == 1 && IsOperator(item[0]))
             {
                 var b = stack.Pop();
                 var a = stack.Pop();
-                stack.Push(item switch
+                stack.Push(item[0] switch
                 {
                     '+' => a + b,
                     '-' => a - b,
@@ -99,11 +119,59 @@ public class Evaluator
             }
             else
             {
-                stack.Push(double.Parse(item.ToString()));
+                stack.Push(ParseNumber(item));
             }
         }
+
         return stack.Pop();
     }
 
     private static bool IsOperator(char item) => "+-*/^()".Contains(item);
+
+    private static IEnumerable<string> Tokenize(string infix)
+    {
+        var currentNumber = string.Empty;
+
+        foreach (var item in infix.Replace(',', '.'))
+        {
+            if (char.IsWhiteSpace(item))
+            {
+                continue;
+            }
+
+            if (char.IsDigit(item) || item == '.')
+            {
+                currentNumber += item;
+                continue;
+            }
+
+            if (currentNumber.Length > 0)
+            {
+                yield return currentNumber;
+                currentNumber = string.Empty;
+            }
+
+            if (!IsOperator(item))
+            {
+                throw new Exception("Sintax error.");
+            }
+
+            yield return item.ToString();
+        }
+
+        if (currentNumber.Length > 0)
+        {
+            yield return currentNumber;
+        }
+    }
+
+    private static double ParseNumber(string item)
+    {
+        if (double.TryParse(item, NumberStyles.Float, CultureInfo.InvariantCulture, out var result))
+        {
+            return result;
+        }
+
+        throw new Exception("Sintax error.");
+    }
 }
